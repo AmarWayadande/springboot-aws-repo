9ba162acd81a762b59823d56d04a2103e72070d1
Shreetej <amarwayadande4231@gmail.com>
2024-09-05 13:45:23 +0530

    first commit

---
+++ springboot-aws-ecs-new/src/main/java/com/example/springboot_aws_ecs_new/SpringbootAwsEcsNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_aws_ecs_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsEcsNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsEcsNewApplication.class, args);
+	}
+
+}
---
+++ springboot-aws-ecs-new/src/test/java/com/example/springboot_aws_ecs_new/SpringbootAwsEcsNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_aws_ecs_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsEcsNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
