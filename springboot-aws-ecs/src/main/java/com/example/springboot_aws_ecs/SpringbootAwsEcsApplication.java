498a5bea6e336222b505ec17f8b4985bb805e1c9
Shreetej <amarwayadande4231@gmail.com>
2024-09-05 13:19:23 +0530

    first commit

---
+++ springboot-aws-ecs/src/main/java/com/example/springboot_aws_ecs/SpringbootAwsEcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_aws_ecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsEcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsEcsApplication.class, args);
+	}
+
+}
